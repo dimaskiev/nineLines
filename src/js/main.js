941e64b05b65ec0478f0243525a88d525e4e9e3b
dimaskiev <manager.ua@gmail.com>
2018-12-15 22:05:21 +0100

    markup & styling blocks

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
 import './vendor';
+
+
+$(".skills__item").click(function(e) {
+    $(this).children('.checkbox__fake').toggleClass('checked');
+})
+
+
+$(".slider-img").mousemove(function(e) {
+    
+    var pos = $(this).offset();
+    var elem_left = pos.left;
+    var elem_top = pos.top;
+    // положение курсора внутри элемента
+    var Xinner = e.pageX - elem_left;
+    var Yinner = e.pageY - elem_top;
+    console.log(Xinner + " " + Yinner);
+})
